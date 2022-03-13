411b39b76e17176e33b26198db8504b70c820e01
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2022-03-13 21:37:03 +0500

    Fixed evaluation code

--- functions.py
+++ functions.py
@@ -12,13 +12,31 @@ def PLU(x, alpha = 0.1, c = 1.0):
     return o4
 
 
-def gen_ztta(dim = 256, length = 50):
-    ### currently without T_max ###
-    ztta = np.zeros((1, length, dim))
-    for t in range(length):
+# def gen_ztta(dim = 256, length = 50):
+#     ### currently without T_max ###
+#     ztta = np.zeros((1, length, dim))
+#     for t in range(length):
+#         for d in range(dim):
+#             if d % 2 == 0:
+#                 ztta[:, t, d] = np.sin(1.0 * (length - t) / 10000 ** (d / dim))
+#             else:
+#                 ztta[:, t, d] = np.cos(1.0 * (length - t) / 10000 ** (d / dim))
+#     return torch.from_numpy(ztta.astype(float))
+
+
+
+
+def gen_ztta(timesteps=60, dim=256):
+    ztta = np.zeros((timesteps, dim))
+    for t in range(timesteps):
         for d in range(dim):
             if d % 2 == 0:
-                ztta[:, t, d] = np.sin(1.0 * (length - t) / 10000 ** (d / dim))
+                ztta[t, d] = np.sin(t / (10000 ** (d // dim)))
             else:
-                ztta[:, t, d] = np.cos(1.0 * (length - t) / 10000 ** (d / dim))
-    return torch.from_numpy(ztta.astype(np.float))
+                ztta[t, d] = np.cos(t / (10000 ** (d // dim)))
+    return torch.from_numpy(ztta.astype(float))
+
+
+if __name__ == "__main__":
+    ztta = gen_ztta(length=60)
+    print(ztta.shape)
