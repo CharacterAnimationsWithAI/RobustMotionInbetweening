7fd7c9ad6d7ed49900c14947c24623199299f9eb
Usama Ahmed Siddiquie <220usamaahmed@gmail.com>
2022-03-15 00:14:05 +0500

    Cleaned file

--- functions.py
+++ functions.py
@@ -12,20 +12,6 @@ def PLU(x, alpha = 0.1, c = 1.0):
     return o4
 
 
-# def gen_ztta(dim = 256, length = 50):
-#     ### currently without T_max ###
-#     ztta = np.zeros((1, length, dim))
-#     for t in range(length):
-#         for d in range(dim):
-#             if d % 2 == 0:
-#                 ztta[:, t, d] = np.sin(1.0 * (length - t) / 10000 ** (d / dim))
-#             else:
-#                 ztta[:, t, d] = np.cos(1.0 * (length - t) / 10000 ** (d / dim))
-#     return torch.from_numpy(ztta.astype(float))
-
-
-
-
 def gen_ztta(timesteps=60, dim=256):
     ztta = np.zeros((timesteps, dim))
     for t in range(timesteps):
@@ -37,6 +23,5 @@ def gen_ztta(timesteps=60, dim=256):
     return torch.from_numpy(ztta.astype(float))
 
 
-if __name__ == "__main__":
-    ztta = gen_ztta(length=60)
-    print(ztta.shape)
+def save_bvh_from_network_output(rotations, output_path):
+    pass
